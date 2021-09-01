02a31309c38fe0f8f16158b34966d6217ae8621f
plshazer <pls@hazear.com>
2021-09-01 20:35:59 -0300

    bootstrap for navbar

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
-import NavBar from './components/NavBar';
+import NavBar from './components/NavBar.js';
+
+
 
 
 
 function App() {
   return (
     <div className="App">      
-      <NavBar />
-
+  <NavBar/>
     </div>
   );
 }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,12 +1,20 @@
-import { directive } from "@babel/types"
+import { Navbar,Nav,NavDropdown,Form,FormControl,Button, Container } from 'react-bootstrap'
 
 
 function NavBar() {
     return (
-<div>
-    
-</div>
-    
+        <div>
+       <Navbar bg="dark" variant="dark">
+    <Container>
+    <Navbar.Brand href="#home">DevBar</Navbar.Brand>
+    <Nav className="me-auto">
+      <Nav.Link href="#home">Inicio</Nav.Link>
+      <Nav.Link href="#features">Categorias</Nav.Link>
+      <Nav.Link href="#pricing">Contactanos</Nav.Link>
+    </Nav>
+    </Container>
+  </Navbar>
+        </div>
     )
 }
 
