a0041872f237ef8de1122306afb71ec3021fcd9d
plshazer <pls@hazear.com>
2021-09-30 02:17:30 -0300

    env usage

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -3,13 +3,13 @@ import firebase from "firebase/compat/app";
 import "firebase/compat/firestore"
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDdyUN4c_zneF-A4rU3fcmnp4gytuZthX8",
-  authDomain: "devbar-react.firebaseapp.com",
-  projectId: "devbar-react",
-  storageBucket: "devbar-react.appspot.com",
-  messagingSenderId: "379749534531",
-  appId: "1:379749534531:web:09d4c2e2b388dd60233df6",
-  measurementId: "G-2HZ6KEB5DP"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID
 };
 
 // Initialize Firebase
